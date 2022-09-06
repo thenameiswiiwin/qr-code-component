319bff1d232ab3b85d4d3e3cbd0eca3b7769d31b
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-09-06 16:23:49 -0500

    styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
+import QR from '../assets/images/image-qr-code.png';
+
 function App() {
   return (
-    <div>
-      Hello
+    <div className="h-screen flex justify-center items-center bg-lightGray">
+      <div className="max-w-[550px] mx-9 flex flex-col p-7 bg-white rounded-3xl gap-10">
+        <img className="rounded-xl" src={QR} alt="qr code" />
+        <div className="flex flex-col px-5 text-center font-outfit gap-6">
+          <h2 className='text-3xl font-bold text-darkBlue'>Improve your front-end skills by building projects</h2>
+          <p className='text-2xl text-grayishBlue mb-7'>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)',
+      },
+    },
   },
   plugins: [],
-}
+};
